d63e51566b8ea707a6ecaa984511fab7329241d0
David Vargas <davids10.dvm@gmail.com>
2024-12-16 13:04:51 -0500

    Fix: solved problems with the deployment

--- src/app/utils/parseError.ts
+++ src/app/utils/parseError.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export const parseError = (error: any) => {
-    if (error?.response?.data?.error?.includes("URI query for nonexistent token")) {
-      return "The given NFT metadata has not been minted.";
-    }
-    return "An unexpected error occurred.";
-  };
-  
+  if (error?.response?.data?.error?.includes("URI query for nonexistent token")) {
+    return "The given NFT metadata has not been minted.";
+  }
+  return "An unexpected error occurred.";
+};
